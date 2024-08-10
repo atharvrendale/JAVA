28e290f1ff750784e4831a7942bba1721ccec6b7
atharvrendale <165877195+atharvrendale@users.noreply.github.com>
2024-08-10 12:55:03 +0530

    Create info.java

---
+++ info.java
@@ -0,0 +1,12 @@
+class me
+{
+public static void main(String args[])
+{
+System.out.println("Atharv Gajanan Rendale");
+System.out.println("9579958053");
+System.out.println("22UAI108");
+System.out.println("CSE AI");
+System.out.println("Vivekanand nagar korochi");
+
+}
+}
